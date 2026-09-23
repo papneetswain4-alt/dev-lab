f984fd649fb92a3464d0bff0994c42e6022ea15a
Papneet Swain <papneetswain4@gmail.com>
2026-09-23 20:49:07 +0530

    Update 3498-reverse-degree-of-a-string.java

--- problems/3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
+++ problems/3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -1,12 +1,9 @@
 class Solution {
     public int reverseDegree(String s) {
-        String reversedAlphabet = "zyxwvutsrqponmlkjihgfedcba";
         int sum = 0;
-        for(int i = 0 ; i < s.length() ; i++){
-            char c = s.charAt(i);
-            int reversedIndex = reversedAlphabet.indexOf(c);
-            int product = (i+1)*(reversedIndex + 1);
-            sum += product;
+        for (int i = 0; i < s.length(); i++) {
+            int reversedIndex = 'z' - s.charAt(i) + 1;
+            sum += (i + 1) * reversedIndex ;
         }
         return sum;
     }
