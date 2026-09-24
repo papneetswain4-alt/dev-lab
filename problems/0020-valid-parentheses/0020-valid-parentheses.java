a76f4f558311231efcfeb8a47314732d23e0ba3a
Papneet Swain <papneetswain4@gmail.com>
2026-09-24 21:22:37 +0530

    Time: 3 ms (86.1%), Space: 42.8 MB (98.05%) - LeetHub-Neo

---
+++ problems/0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0 ; i < s.length() ; i++){
+            char c = s.charAt(i);
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            }else{
+                if(stack.empty()){
+                    return false ;
+                }
+                char topch = stack.peek();
+                stack.pop();
+                if( c == ')' && topch == '(' || c == ']' && topch == '[' || c == '}' && topch == '{'){
+                    continue;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+        
+    }
+}
